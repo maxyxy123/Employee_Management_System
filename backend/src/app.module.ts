f73fd4b7ed3a96dd76444757f6604571c6eb9457
maxyxy123 <maxminh716@gmail.com>
2026-06-10 19:33:50 +0700

    init backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { EmployeesModule } from './employees/employees.module';
+import { DepartmentsModule } from './departments/departments.module';
+import { LeavesModule } from './leaves/leaves.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, EmployeesModule, DepartmentsModule, LeavesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
