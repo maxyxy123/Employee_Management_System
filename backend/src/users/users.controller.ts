a782bfb342bbc0ac1a3aa93eaa79c749dc478168
maxyxy123 <maxminh716@gmail.com>
2026-06-16 07:27:39 +0700

    Users services

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UpdatePasswordDto, UpdateRoleDto, UpdateStatusDto } from 'src/dto/user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getAllUser() {
+    return this.usersService.getAllUser();
+  }
+
+  @Get(':id')
+  getOneUser(@Param('id') id: string) {
+    return this.usersService.getOneUser(id);
+  }
+
+  @Put(':id/role')
+  updateRole(@Param('id') id: string, @Body() updateRole: UpdateRoleDto) {
+    return this.usersService.updateRole(id, updateRole);
+  }
+
+  @Put(':id/status')
+  updateStatus(@Param('id') id: string, @Body() updateStatus: UpdateStatusDto) {
+    return this.usersService.updateStatus(id, updateStatus);
+  }
+
+  @Put(':id/password')
+  updatePassword(
+    @Param('id') id: string,
+    @Body() updatePassword: UpdatePasswordDto,
+  ) {
+    return this.usersService.updatePassword(id, updatePassword);
+  }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,142 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import {
+  UpdatePasswordDto,
+  UpdateRoleDto,
+  UpdateStatusDto,
+} from 'src/dto/user.dto';
+import bcrypt from 'bcrypt';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllUser() {
+    const allUser = await this.prisma.user.findMany({
+      where: { status: 'ACTIVE' },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        status: true,
+        createdAt: true,
+      },
+    });
+    if (allUser.length === 0)
+      throw new NotFoundException('No active user found');
+
+    return {
+      meassage: 'Successfully get all Active User',
+      data: allUser,
+    };
+  }
+  async getOneUser(userId: string) {
+    if (!userId) {
+      throw new BadRequestException('User id is required');
+    }
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId, status: 'ACTIVE' },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        status: true,
+        createdAt: true,
+      },
+    });
+    if (!user) throw new NotFoundException('User not found');
+    return {
+      message: 'Successfully found user',
+      data: user,
+    };
+  }
+
+  async updateRole(userId: string, updateRole: UpdateRoleDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!user) throw new NotFoundException('User not found');
+
+    const updatedRoleForUser = await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        role: updateRole.role,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        role: true,
+        status: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    return {
+      message: 'Successfully update User Role',
+      data: updatedRoleForUser,
+    };
+  }
+
+  async updateStatus(userId: string, updateStatus: UpdateStatusDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    const updatedUserStatus = await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        status: updateStatus.status,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        role: true,
+        status: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    return {
+      message: 'Successfully update Status',
+      data: updatedUserStatus,
+    };
+  }
+
+  async updatePassword(userId: string, updatePassword: UpdatePasswordDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!user) throw new NotFoundException('User not found');
+
+    const hashPassword = await bcrypt.hash(updatePassword.password, 12);
+
+    const updatePasswordForUser = await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        password: hashPassword,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        role: true,
+        status: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    return {
+      message: 'Successfully change Password',
+      data: updatePasswordForUser,
+    };
+  }
+}
