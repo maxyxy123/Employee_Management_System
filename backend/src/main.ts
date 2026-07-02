8cf5060e5024a8b899bd62eb61c23801a917e472
maxyxy123 <maxminh716@gmail.com>
2026-07-02 21:51:24 +0700

    add frontend project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,10 @@ import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +16,6 @@ async function bootstrap() {
     }),
   );
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
